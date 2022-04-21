0412f2b2472d32d68fb5bf694a0425e75b73f9d5
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-21 11:52:52 +0700

    add templates

--- food/models.py
+++ food/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Item(models.Model):
-    # เวลาโชว์ list ของ obj จะให้แสดงเป็นค่่าอะไร (df เป๋น pk )
+    # เวลาโชว์ list ของ obj จะให้แสดงเป็นค่่าอะไร (df เป๋น pk)
     def __str__(self):
         return self.item_name
 
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
-    item_price = models.IntegerField(max_length=100)
+    item_price = models.IntegerField()
--- food/views.py
+++ food/views.py
@@ -1,6 +1,24 @@
+from multiprocessing import context
+from urllib import request
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Item
 
 # Create your views here.
-def index(response): 
-    return HttpResponse('hello')
+def index(request): 
+    Item_list = Item.objects.all()
+    context = {
+        'Item_list'  : Item_list 
+    }
+    return render(request, 'food/index.html' , context )
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item
+    }
+    return render(request, 'food/detail.html' , context )
+
+def item(request):
+    return HttpResponse('<h1> item views </h1>')
