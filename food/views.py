c81dd881453565ac30878f1c10b5aee9abc4a487
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-22 12:40:23 +0700

    forms app update delete

--- food/views.py
+++ food/views.py
@@ -1,9 +1,11 @@
 from multiprocessing import context
 from urllib import request
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.template import loader
+
 from .models import Item
+from .forms import ItemForm 
 
 # Create your views here.
 def index(request): 
@@ -22,3 +24,39 @@ def detail(request, item_id):
 
 def item(request):
     return HttpResponse('<h1> item views </h1>')
+
+def create_item(request):
+    form = ItemForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    context = {
+        'form':form
+    }
+    return render(request, 'food/item-form.html', context  )
+
+def update_item(request, id):
+    item = Item.objects.get(pk=id)
+    form = ItemForm(request.POST or None, instance=item)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+    context = {
+        'form':form
+    }
+    return render(request, 'food/item-form.html', context  )
+
+def delete_item(request, id):
+    print(id)
+    item = Item.objects.get(pk=id)
+    if request.method == 'POST': 
+        item.delete()
+        return redirect('food:index')
+    context = {
+        'item':item
+    }
+    return render(request, 'food/item-delete.html', context )
+
+    
