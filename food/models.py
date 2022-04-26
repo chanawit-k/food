da2fbc2d5b168f380982143412510006feff3a5e
Chanawit.k <chanawit.kaewpaluek@gmail.com>
2022-04-26 15:30:58 +0700

    class base view createview

--- food/models.py
+++ food/models.py
@@ -1,12 +1,16 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
 class Item(models.Model):
     # เวลาโชว์ list ของ obj จะให้แสดงเป็นค่่าอะไร (df เป๋น pk)
     def __str__(self):
         return self.item_name
-
+    user_name = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
     item_img = models.CharField(max_length=500, default="https://image.shutterstock.com/image-vector/dish-sketch-vector-illustration-isolated-260nw-582588370.jpg")
+
+    def get_absolute_url(self):
+        return reverse("food:detail", kwargs={"pk": self.pk})
